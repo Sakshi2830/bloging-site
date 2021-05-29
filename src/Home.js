44a615a3a77ebd0c3fe4e08552578f7a2224d033
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-29 21:42:22 +0530

    fetching local data

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,43 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  const [blog,setBlog] = useState([
-      {title: 'MY NEW website', body: 'lorem epsum.......', author: 'mario'},
-      {title: 'welome website', body: 'lorem epsum.......', author: 'marin'},
-      {title: 'newone website', body: 'lorem epsum.......', author: 'martha'}
-  ])
+  const [blogs,setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
+
+
+  useEffect(() =>{
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then( res =>{
+        if(!res.ok){
+          throw Error('could not fetch');
+        }
+        return res.json();
+       })
+       .then(data => {
+         setBlogs(data);
+         setIsPending(false)
+         setError(null)
+       })
+       .catch((err) =>{
+        setIsPending(false)
+         setError(err.message)
+       })
+      }, 1000);
+  },[]);
+//"},[])"this empty array dependency make sure that this hooks  only runs the function after the first initial render,there after if the state changes it dont run the function
+  // const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter(blog => blog.id !== id);
+  //     setBlogs(newBlogs);
+  // }
     return (  
         <div className="home">
-            <BlogList  blogs={blog} title="All Blog!" />
+          { error && <div>{error}</div>}
+          { isPending && <div>Loading.......</div>}
+            {blogs && <BlogList  blogs={blogs} title="All Blog!"  />}
          </div>
     );
 }
  
-export default Home
+export default Home;
