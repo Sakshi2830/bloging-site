bd27058c8308ea33e9f66acf44a6f6add284b759
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-30 22:17:00 +0530

    final commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import {Link } from 'react-router-dom';
 const Navbar = () => {
     return (  
         <nav className="navbar">
-            <h1>The Blogging Blog</h1>
+            <h1>Write Your Thoughts</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
